22d75fd74b151eb90594c23a585795fe71c05755
Ryan King <hello@ryanking.com>
2018-08-03 09:22:06 -0400

    Setup basic backend webserver with healthcheck

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"flag"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
+)
+
+var (
+	debug = flag.Bool("debug", false, "Show debug information")
+)
+
+func ini() {
+	flag.Parse()
+	if *debug {
+		log.SetLevel(log.DebugLevel)
+	} else {
+		log.SetLevel(log.InfoLevel)
+	}
+
+	log.SetFormatter(new(log.TextFormatter))
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	log.Info("Starting atsregistry...")
+	r := mux.NewRouter()
+	r.HandleFunc("/healthcheck", healthCheck).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode("Still alive!")
 }
